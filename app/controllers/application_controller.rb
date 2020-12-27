341f3f01229d11b8ee6af2299a31134a72d3ee93
kokemaru2427 <kai0405kai0407@icloud.com>
2020-12-27 16:22:28 +0900

    User管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:encrypted_password, :nickname, :family_name, :last_name, :j_family_name, :j_last_name, :birthday])
+  end
 end
+  
---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password,:password,:password_confirmation,length:{maximum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :password, confirmation: true
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :j_family_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :j_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
+  has_many :items
+  has_many :purchases
+end
