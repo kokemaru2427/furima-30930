26fbc17f78e15bf152b4c3cb55ad5b394abfbda9
kokemaru2427 <kai0405kai0407@icloud.com>
2021-01-02 17:44:02 +0900

    ユーザー管理機能の修正10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:encrypted_password, :nickname, :family_name, :last_name, :j_family_name, :j_last_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :family_name, :last_name, :j_family_name, :j_last_name, :birthday])
   end
 end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :email, presence: true, uniqueness: true
   validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  # validates :password, confirmation: true
   with_options presence: true do
     validates :nickname
     validates :birthday
 
-  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
     validates :family_name
     validates :last_name
   end
