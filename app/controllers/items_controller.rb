6dbf0d96406d5835c75766c2738fda7a03563bc2
kokemaru2427 <kai0405kai0407@icloud.com>
2021-01-21 14:08:36 +0900

    商品情報編集機能の修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :move_to_index_item]
   before_action :move_to_index_item, only: :edit
   
@@ -41,12 +42,6 @@ class ItemsController < ApplicationController
 
   def product_params
     params.require(:item).permit(:name, :explanation, :category_id, :state_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 
   def move_to_index_item
